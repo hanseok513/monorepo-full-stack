0f967ad2dd0b79c7def610290cd57dcaafec249d
hanseok513 <hanseok513@gmail.com>
2023-09-02 19:37:41 +0900

    Fix some config to run cloud functions

--- apps/was/functions/src/index.ts
+++ apps/was/functions/src/index.ts
@@ -7,8 +7,14 @@
  * See a full list of supported triggers at https://firebase.google.com/docs/functions
  */
 
-import {onRequest} from "firebase-functions/v2/https";
-import * as logger from "firebase-functions/logger";
+import { onRequest } from "firebase-functions/v1/https";
+
+export const test = onRequest(async (req, res) => {
+  console.log(req);
+  res.status(200).json({ message: "Hello from Firebase!" });
+});
+
+export default test;
 
 // Start writing functions
 // https://firebase.google.com/docs/functions/typescript
